69d653e07374f4784dd99f4cb54114a4a140f1a2
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-07-20 09:08:11 -0300

    Criando mock para posts

--- src/components/Posts/Post.spec.jsx
+++ src/components/Posts/Post.spec.jsx
@@ -1,8 +1,34 @@
 import {render} from '@testing-library/react';
+
 import {Posts} from '.';
+
+// passando parametros para testes, sao chamados de mocks, parametros usados apenas para testes
+const props = {
+    posts: [
+        {
+            id: 1,
+            title: 'title1',
+            body: 'body1',
+            cover: 'img/img.png'
+        },
+        {
+            id: 2,
+            title: 'title2',
+            body: 'body2',
+            cover: 'img/img.png'
+        },
+        {
+            id: 3,
+            title: 'title3',
+            body: 'body3',
+            cover: 'img/img.png'
+        },
+    ]
+};
 
 describe('<Posts />', () => {
     it('should render posts', () => {
-        render(<Posts />)
+       const {debug} = render(<Posts {...props} />)
+        debug();
     });
 });
