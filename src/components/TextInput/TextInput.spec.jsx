b7df2dda1d3926dc804fa0bef01e708d14ad98e8
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-07-22 13:53:35 -0300

    Testando se o que estamos passando esta correto e a quantidades de cliques

--- src/components/TextInput/TextInput.spec.jsx
+++ src/components/TextInput/TextInput.spec.jsx
@@ -1,4 +1,6 @@
 import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import {TextInput} from '.';
 
 // passando parametros para testes, sao chamados de mocks, parametros usados apenas para testes
@@ -7,14 +9,27 @@ describe('<TextInput />', () => {
     it('should have a value of searchValue', () => {
         const fn = jest.fn();
         render(<TextInput handleChange={fn} searchValue={'Funfano'} />)
+        
         const input = screen.getByPlaceholderText(/Digite a sua pesquisa/i);
-        expect(input).toBeInTheDocument();
+        expect(input).toBeInTheDocument();// verifica se o que queremos esta no documento
+        
     
     });
 
 
     it('should call handleChange function on each key pressed', () => {
-        const fn = jest.fn();
-        render(<TextInput handleChange={fn} searchValue={'Testando'}/>);
+        const fn = jest.fn();// cria uma funcao imaginaria para testes
+        render(<TextInput handleChange={fn} />);
+
+        const input = screen.getByPlaceholderText(/Digite a sua pesquisa/i); //verifica se o que o usuario esta vendo é o que queremos
+
+        const value = 'o valor';
+        
+        //userEvent.type recebe dois parametros, o primeiro é a entrada,
+        //e depois o valor que ela vai receber para teste
+        userEvent.type(input, value);
+        
+        expect(input.value).toBe(value);
+        expect(fn).toHaveBeenCalledTimes(value.length)// verifica quantas vezes a variavel foi chamada
     });
 });
