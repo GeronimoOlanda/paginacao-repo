44519527c73aeafb07db00494db23ce3fe5b4595
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-07-22 13:42:49 -0300

    Criando a variavel e pegando ela  para depois retorna-la no expect

--- src/components/TextInput/TextInput.spec.jsx
+++ src/components/TextInput/TextInput.spec.jsx
@@ -6,7 +6,12 @@ import {TextInput} from '.';
 describe('<TextInput />', () => {
     it('should have a value of searchValue', () => {
         const fn = jest.fn();
+        render(<TextInput handleChange={fn} searchValue={'Funfano'} />)
+        const input = screen.getByPlaceholderText(/Digite a sua pesquisa/i);
+        expect(input).toBeInTheDocument();
+    
     });
+
 
     it('should call handleChange function on each key pressed', () => {
         const fn = jest.fn();
