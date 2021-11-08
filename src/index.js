36f5306d42fdab50be096de4b521e004f2d5129d
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-08 13:55:47 -0300

    construindo meu hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './templates/Home';
+import { Home } from './templates/Home';
 import './styles/global-styles.css';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
