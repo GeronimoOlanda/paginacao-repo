67ae4460f9c65787668248f0554036e46c3fcc34
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-09 10:29:31 -0300

    criando o useFetch e suas dependencias

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,3 +1,32 @@
+/* eslint-desable */
+
+//primeiro paramtro da funcao vai ser sempre uma URL ou um OBJETO
+
+import { useEffect, useState } from 'react';
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const jsonResult = await response.json();
+        setResult(jsonResult);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        throw e;
+      }
+    };
+  }, [url]);
+
+  return [1, 2];
+};
 export const Home = () => {
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts');
+  console.log(result, loading);
   return <h1>Oi</h1>;
 };
