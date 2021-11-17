841c06ce62b26b2df3e57f213cc2b755b70ec567
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-11-17 10:13:49 -0300

    trabalhando com matchMedia

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from 'react';
+const useMediaQuery = (queryValue) => {
+  const [match, setMatch] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(Boolean(matchMedia.matches));
+    };
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    //funcao executada quando o componente desmonta
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+  return match;
+};
 export const Home = () => {
-  return <h1> Ola </h1>;
+  const huge = useMediaQuery('(min-width: 980px)');
+  let background = huge ? 'green' : null;
+  return <h1 style={{ fontSize: '60px', background }}> Ola </h1>;
 };
